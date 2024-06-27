5e5e10942b75e9394621f4ee2f996a4a513f7d64
Thota Sri Naga Mounica <thotasrinagamounika@gmail.com>
2024-06-27 08:18:22 +0530

    test both get and post request

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ app = Flask(__name__)
 api = Api(app)
 
 shorts_post_args = reqparse.RequestParser()
-shorts_post_args.add_argument("title", type=str, help="Title of the short")
-shorts_post_args.add_argument("summary", type=str, help="Summary of the news")
-shorts_post_args.add_argument("srcLink", type=str, help="Orginal Source of the news article")
+shorts_post_args.add_argument("title", type=str, help="Title of the short", required = True)
+shorts_post_args.add_argument("summary", type=str, help="Summary of the news", required = True)
+shorts_post_args.add_argument("srcLink", type=str, help="Orginal Source of the news article", required = True)
 
 
-shorts = {
-    1: {"title": "India won the T20 world cup", "summary": "India won the world cup with a tough fight in T20.", "srcLink": "http://newsarticle.com"},
-    2: {"title": "Nvidia stocks reached market high", "summary": "Nvidia stocks became the most valuable stocks exceeding the Alphabet stocks", "srcLink": "http://newsarticle.com"},
-    3: {"title": "Self-driving cars Operational in US", "summary": "Now you can go on a rider without a driver. Self-driving cars are operational in US", "srcLink": "http://newsarticle.com"},
-}
+# shorts = {
+#     1: {"title": "India won the T20 world cup", "summary": "India won the world cup with a tough fight in T20.", "srcLink": "http://newsarticle.com"},
+#     2: {"title": "Nvidia stocks reached market high", "summary": "Nvidia stocks became the most valuable stocks exceeding the Alphabet stocks", "srcLink": "http://newsarticle.com"},
+#     3: {"title": "Self-driving cars Operational in US", "summary": "Now you can go on a rider without a driver. Self-driving cars are operational in US", "srcLink": "http://newsarticle.com"},
+# }
+
+shorts = {}
 
 class Shorts(Resource):
     def get(self, short_id):
@@ -22,7 +24,8 @@ class Shorts(Resource):
     
     def post(self, short_id):
         args = shorts_post_args.parse_args()
-        return {short_id: args}
+        shorts[short_id] = args
+        return shorts[short_id], 201
     
  
 api.add_resource(Shorts, "/shorts/<int:short_id>")
--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE+"shorts/4", {"title": "Gold prices reach all time high", "summary": "Amid market uncertainities gold prices reach all time high"})
+response = requests.post(BASE+"shorts/4", {"title": "Gold prices reach all time high", "summary": "Amid market uncertainities gold prices reach all time high", "srcLink": "http://newsarticle.com"})
 print(response.json())
+
+input()
+
+response = requests.get(BASE+"shorts/4")
+print(response.json())
+
