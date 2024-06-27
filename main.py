9a1a58beb6239461664f301f444cfbe4da42f85a
Thota Sri Naga Mounica <thotasrinagamounika@gmail.com>
2024-06-27 08:09:40 +0530

    adding the request parser

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+shorts_post_args = reqparse.RequestParser()
+shorts_post_args.add_argument("title", type=str, help="Title of the short")
+shorts_post_args.add_argument("summary", type=str, help="Summary of the news")
+shorts_post_args.add_argument("srcLink", type=str, help="Orginal Source of the news article")
+
+
 shorts = {
-    1: {"title": "India won the T20 world cup", "summary": "India won the world cup with a tough fight in T20."},
-    2: {"title": "Nvidia stocks reached market high", "summary": "Nvidia stocks became the most valuable stocks exceeding the Alphabet stocks"},
-    3: {"title": "Self-driving cars Operational in US", "summary": "Now you can go on a rider without a driver. Self-driving cars are operational in US"},
+    1: {"title": "India won the T20 world cup", "summary": "India won the world cup with a tough fight in T20.", "srcLink": "http://newsarticle.com"},
+    2: {"title": "Nvidia stocks reached market high", "summary": "Nvidia stocks became the most valuable stocks exceeding the Alphabet stocks", "srcLink": "http://newsarticle.com"},
+    3: {"title": "Self-driving cars Operational in US", "summary": "Now you can go on a rider without a driver. Self-driving cars are operational in US", "srcLink": "http://newsarticle.com"},
 }
 
 class Shorts(Resource):
@@ -15,8 +21,8 @@ class Shorts(Resource):
         return shorts[short_id]
     
     def post(self, short_id):
-        print(request.form)
-        return shorts
+        args = shorts_post_args.parse_args()
+        return {short_id: args}
     
  
 api.add_resource(Shorts, "/shorts/<int:short_id>")
